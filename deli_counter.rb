2eb14d8fef05f7d42657ca51ae7dd68aff476e1f
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-10 16:48:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+def line
+
+end
+
+def take_a_number(line, name)
+  line.push(name)
+  puts "Welcome, #{name}. You are number #{line.size} in line."
+end
+
+def now_serving(line)
+  puts "Currently serving #{line[0]}."
