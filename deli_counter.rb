e8dba1ed55ce46f135040ebaa28e0015e810172a
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-10 16:51:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 # Write your code here.
 katz_deli = []
-def line
-
+def line(queue)
+  string = "The line is currently:"
+  queue.each {|name| string << " #{queue.index(name) + 1}. #{name}"}
 end
 
 def take_a_number(line, name)
@@ -11,3 +12,6 @@ end
 
 def now_serving(line)
   puts "Currently serving #{line[0]}."
+end
+
+
